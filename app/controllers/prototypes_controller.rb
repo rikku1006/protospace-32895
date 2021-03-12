d05de6e2b9b87bbbbf1856efca840b9b0ce0e706
rikku1006 <r1006s0125@icloud.com>
2021-03-12 21:01:55 +0900

    ログインユーザー以外の、投稿を直接URLを入力しても遷移出来ないよう修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit,:destroy]
-  before_action :move_to_index, except: [:index, :show,]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     
@@ -53,7 +53,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
       redirect_to action: :index
     end
   end
